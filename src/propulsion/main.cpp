7500dbf0d76a74d31cb46ae2793622de68e5011f
Carlos <pereyrazcarlos@gmail.com>
2022-03-03 18:49:33 -0800

    cfmactu run smoothly now.

---
+++ src/propulsion/main.cpp
@@ -0,0 +1,30 @@
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <iostream> // std
+#include <iomanip>  // setw
+#include <fstream>  // fopen
+#include <string>
+#include <math.h>   // pow
+
+#include "cfmactu.hpp"
+
+/*
+ * compile:
+ * 	g++ -Xpreprocessor -fopenmp -framework Accelerate -o test main.cpp cfmactu.cpp
+ * 	g++ -o test main.cpp cfmactu.cpp
+ *  ./test
+ *	valgrind --leak-check=yes ./test
+ */
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cfmactu *cfm = new cfmactu(argc, argv);
+
+    cfm->readInputParams(argc, argv);
+    cfm->thrustCalc();
+    cfm->outputVT();
+
+    delete cfm;
+}
